2dcf3ce09ab3a3d74542df6ba88badbf994b9468
Craig <craig@fybre.me>
2026-07-15 08:45:42 +1000

    Initial Commit

---
+++ VideoCompressor/VideoCompressorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCompressorApp.swift
+//  VideoCompressor
+//
+//  Created by Craig on 15/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct VideoCompressorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
